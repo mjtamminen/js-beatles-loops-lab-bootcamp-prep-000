5e337cdb68cdc03ec7be416f965a830dbe6451d2
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-16 09:59:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function theBeatlesPlay(musicians, instruments) {
 
 
 function johnLennonFacts(facts) {
-  const facts = [
+  var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
