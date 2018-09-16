7bb545bc3d41a0abda2f7bc2984e5fcbb66756f5
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-16 09:51:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function theBeatlesPlay(musicians, instruments) {
   return newArray
 }
 
-const facts = [
+var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
